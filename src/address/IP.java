0ab86276878fdee9f364f57060941dccbb556d11
Xample <hannes.schniz@gmx.de>
2022-02-15 12:52:36 +0100

    implemented constructor, tostring and compareto

--- src/address/IP.java
+++ src/address/IP.java
@@ -2,16 +2,32 @@ package src.address;
 
 public class IP implements Comparable<IP> {
 
+    private int[] address;
+
     public IP(final String pointNotation) throws ParseException {
+        this.address = Parser.parseToAddress(pointNotation);
     }
 
     @Override
     public String toString() {
-        return null;
+        return Parser.parseToString(address);
     }
 
     @Override
     public int compareTo(IP o) {
+        int[] toCompare = o.getAddress();
+        for (int i = 0; i < address.length; i++) {
+            if (address[i] < toCompare[i]) {
+                return -1;
+            }
+            else if (address[i] > toCompare[i]) {
+                return 1;
+            }
+        }
         return 0;
+    }
+
+    public int[] getAddress() {
+        return address;
     }
 }
