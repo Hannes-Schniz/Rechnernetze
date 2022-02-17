b6d8f79e18a6b4b17c351848344b7ae1f9752690
Xample <hannes.schniz@gmx.de>
2022-02-17 16:51:45 +0100

    finished 3 test cases

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -47,7 +47,7 @@ public class Parser {
         String[] returnString = new String[1];
         for (int i = 0; i < working.length; i++) {
             int pos = 0;
-            if (leadingBracket(working[i]) == 1) {
+            if (distance(working[i]) == 1) {
                 int match = findMatching(working, i);
                 if (i > 0 ) {
                     returnString = extend(returnString);
@@ -57,12 +57,7 @@ public class Parser {
                     if (pos == 0) {
                         returnString[returnString.length - 1] += " " + crop(working[j]);
                     }
-                    if (leadingBracket(working[j]) > 0) {
-                        pos++;
-                    }
-                    else if (trailingBracket(working[j]) > 0) {
-                        pos -= trailingBracket(working[j]);
-                    }
+                    pos += distance(working[j]);
                 }
             }
         }
@@ -70,31 +65,17 @@ public class Parser {
     }
 
     private static String crop(String input) {
-        String returnString = input.substring(leadingBracket(input));
-        returnString = returnString.substring(0, returnString.length() - trailingBracket(returnString));
-        return returnString;
+        int distance = distance(input);
+        if (distance < 0) {
+            return input.substring(0, input.length() + distance);
+        }
+        return input.substring(distance);
     }
 
     private static String[] extend(String[] input) {
         String[] returnValue = new String[input.length + 1];
         System.arraycopy(input, 0, returnValue , 0, input.length);
         return returnValue;
-    }
-
-    private static int leadingBracket(String input) {
-        if (input.charAt(0) == '(') {
-            return 1;
-        }
-        return 0;
-    }
-
-    private static int trailingBracket(String input) {
-        for (int i = input.length() - 1; i > 0; i--) {
-            if (input.charAt(i) != ')') {
-                return input.length() - 1 - i;
-            }
-        }
-        return -1;
     }
 
     private static int distance(String input) {
@@ -104,18 +85,17 @@ public class Parser {
         else {
             for (int i = input.length() - 1; i > 0; i--) {
                 if (input.charAt(i) != ')') {
-                    return input.length() - 1 - i;
+                    return -(input.length() - 1 - i);
                 }
             }
         }
-        return -1;
+        return 0;
     }
 
     private static int findMatching(String[] input, int start) {
         int pos = 0;
         for (int i = start; i < input.length; i++) {
-            pos += leadingBracket(input[i]);
-            pos -= trailingBracket(input[i]);
+            pos += distance(input[i]);
             if (pos <= 0) {
                 return i;
             }
--- test/ParserTest.java
+++ test/ParserTest.java
@@ -13,14 +13,34 @@ class ParserTest {
 
     @org.junit.jupiter.api.Test
     void parseToTree() {
-        String[] correct = {"85.193.148.81 141.255.1.133 34.49.145.239 231.189.0.127"
+        String[] correct1 = {"85.193.148.81 141.255.1.133 34.49.145.239 231.189.0.127"
                 , "141.255.1.133 122.117.67.158 0.146.197.108"
                 , "231.189.0.127 77.135.84.171 39.20.222.120 252.29.23.0 116.132.83.77"};
-        String[] returnString = Parser.parseToTree("(85.193.148.81"
+        String[] correct2 = {"85.193.148.81 141.255.1.133 34.49.145.239 231.189.0.127"};
+        String[] correct3 = {"85.193.148.81 34.49.145.239 141.255.1.133 231.189.0.127",
+            "141.255.1.133 0.146.197.108 122.117.67.158", "231.189.0.127 39.20.222.120 116.132.83.77 252.29.23.0",
+            "39.20.222.120 77.135.84.171"};
+        String[] returnString1 = Parser.parseToTree("(85.193.148.81"
                 + " (141.255.1.133 122.117.67.158 0.146.197.108) 34.49.145.239"
                 + " (231.189.0.127 77.135.84.171 39.20.222.120 252.29.23.0 116.132.83.77))");
-        for (int i = 0; i < correct.length; i++) {
-            assertEquals(correct[i], returnString[i]);
+        String[] returnString2 = Parser.parseToTree("(85.193.148.81 141.255.1.133 34.49.145.239 231.189.0.127)");
+        String[] returnString3 = Parser.parseToTree("(85.193.148.81 34.49.145.239 (141.255.1.133 0.146.197.108"
+                + " 122.117.67.158) (231.189.0.127 (39.20.222.120"
+                + " 77.135.84.171) 116.132.83.77 252.29.23.0))");
+        System.out.println("case 1");
+        for (int i = 0; i < correct1.length; i++) {
+            System.out.println("correct: " + correct1[i] + " |Output: " + returnString1[i]);
+            assertEquals(correct1[i], returnString1[i]);
+        }
+        System.out.println("case 2");
+        for (int i = 0; i < correct2.length; i++) {
+            System.out.println("correct: " + correct2[i] + " |Output: " + returnString2[i]);
+            assertEquals(correct2[i], returnString2[i]);
+        }
+        System.out.println("case 3");
+        for (int i = 0; i < correct3.length; i++) {
+            System.out.println("correct: " + correct3[i] + " |Output: " + returnString3[i]);
+            assertEquals(correct3[i], returnString3[i]);
         }
     }
 }
